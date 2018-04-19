edde72816e8e72fa200d13a495f257348582ed24
admin <admin@iMac3-10.local>
2018-04-19 17:09:55 +0200

    Finished second request Ajax for planets StarWars and added somes less style

--- js/script.js
+++ js/script.js
@@ -17,22 +17,22 @@ $(document).ready(function() {
         document.querySelector('.perso').appendChild(list);
         });
     });
-})
 
-/*$(document).ready(function() {
-    $('#a').on('click', function() {
-        $.get("https://swapi.co/api/people/", function(perso) {
-            let persoList = perso.results;
-            let list = document.createElement('ul');
-            list.classList.add('ul-style');
-                for (let i = 0; i < persoList.lenght; i++) {
-                    let listElement = document.createElement('li');
-                    listElement.innerHTML = persoList[i].name;
-                    listElement.dataset.url = persoList[i].url;
-                    list.appendChild(listElement);  
-                };
-                document.querySelector('.perso').appendChild(list);
-        });
-    });
+    $('#b').on('click', function() {
+        $.get("https://swapi.co/api/planets/", function(planet) {
+          if($('.ul-style').length > 0 ) {
+            $('.ul-style').remove();
+        };
+          let planetList = planet.results;
+          let list = document.createElement('ul');
+          list.classList.add('ul-style');
+          for (let i = 0; i < planetList.length; i++) {
+            let listElement = document.createElement('li');
+            listElement.innerHTML = planetList[i].name;
+            listElement.dataset.url = planetList[i].url;
+            list.appendChild(listElement);
+          };
+          document.querySelector('.planete').appendChild(list);
+          });
+      });
 })
-*/
