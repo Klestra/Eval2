1b5ece2e41f96b551d1712b8539a1ca5346be36f
admin <admin@iMac3-10.local>
2018-04-20 09:39:41 +0200

    Finished the request ajax for the planets

--- js/script.js
+++ js/script.js
@@ -42,12 +42,11 @@ $(document).ready(function() {
         });  
       });
     });
-
     $('#b').on('click', function() {
         $.get("https://swapi.co/api/planets/", function(planet) {
           if($('.ul-style').length > 0 ) {
             $('.ul-style').remove();
-        };
+          };
           let planetList = planet.results;
           let list = document.createElement('ul');
           list.classList.add('ul-style');
@@ -58,14 +57,38 @@ $(document).ready(function() {
             list.appendChild(listElement);
           };
           document.querySelector('.planete').appendChild(list);
-          });
+          $('li').on('click', function() {
+            $.get(this.dataset.url, function(planet) {
+              if($('.ul-style').length > 0 ) {
+                $('.ul-style').remove();
+              }; 
+              list.classList.add('ul-style');
+              let detail = document.createElement('ul');
+              let detailElement1 = document.createElement('li');
+              let detailElement2 = document.createElement('li');
+              let detailElement3 = document.createElement('li');
+              let detailElement4 = document.createElement('li');
+              let detailElement5 = document.createElement('li');
+              detailElement1.innerHTML = "Climat : " + planet.climate;
+              detailElement2.innerHTML = "Diametre : " + planet.diameter;
+              detailElement3.innerHTML = "Population : " + planet.population;
+              detailElement4.innerHTML = "Type de terrain : " + planet.terrain;
+              detailElement5.innerHTML = "Periode orbital : " + planet.orbital_period;
+              detail.appendChild(detailElement1);
+              detail.appendChild(detailElement2);
+              detail.appendChild(detailElement3);
+              detail.appendChild(detailElement4);
+              detail.appendChild(detailElement5);
+              document.querySelector('.planete').appendChild(detail);
+             });
+          }); 
+        });
       });
-
       $('#c').on('click', function() {
         $.get("https://swapi.co/api/starships/", function(starship) {
           if($('.ul-style').length > 0 ) {
             $('.ul-style').remove();
-        };
+          };
           let starshipList = starship.results;
           let list = document.createElement('ul');
           list.classList.add('ul-style');
@@ -76,6 +99,6 @@ $(document).ready(function() {
             list.appendChild(listElement);
           };
           document.querySelector('.vaisseau').appendChild(list);
-        });
+       });
     });
 })
